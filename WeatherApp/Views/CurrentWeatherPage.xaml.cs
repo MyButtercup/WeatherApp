0e50ad6a20e0402edb014f3d93cc3d70a377ba13
Любовь Серкова <serkova3000@gmail.com>
2021-03-13 14:02:36 +0300

    api added

--- WeatherApp/Views/CurrentWeatherPage.xaml.cs
+++ WeatherApp/Views/CurrentWeatherPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WeatherApp.Helper;
+using WeatherApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +17,43 @@ namespace WeatherApp.Views
         public CurrentWeatherPage()
         {
             InitializeComponent();
+            GetWeatherInfo();
+        }
+
+        private string Location = "France";
+
+        private async void GetWeatherInfo()
+        {
+            var url = $"http://api.openweathermap.org/data/2.5/weather?q={Location}&appid=ec8373111e548e416ee4ab0b3b2fbf91&units=metric";
+
+            var result = await ApiCaller.Get(url);
+
+            if (result.Successful)
+            {
+                try 
+                {
+                    var weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(result.Response);
+                    descriptionTxt.Text = weatherInfo.weather[0].description.ToUpper();
+                    iconImg.Source = $"w{weatherInfo.weather[0].icon}";
+                    cityTxt.Text = weatherInfo.name.ToUpper();
+                    temperatureTxt.Text = weatherInfo.main.temp.ToString("0");
+                    humidityTxt.Text = $"{weatherInfo.main.humidity}%";
+                    pressureTxt.Text = $"{weatherInfo.main.pressure} hpa";
+                    windTxt.Text = $"{weatherInfo.wind.speed} m/s";
+                    cloudinessTxt.Text = $"{weatherInfo.clouds.all}%";
+
+                    var dt = new DateTime().ToUniversalTime().AddSeconds(weatherInfo.dt);
+                    dateTxt.Text = dt.ToString("dddd, MMM dd").ToUpper();
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+            else
+            {
+                await DisplayAlert("Weather Info", "No weather information found", "OK");
+            }
         }
     }
 }
